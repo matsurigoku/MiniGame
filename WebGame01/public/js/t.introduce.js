fa84f63f0c12dd7b82316fe3c093f1b7544511cb
matsurigoku <nywh1021x0318@gmail.com>
2022-06-15 05:08:18 +0800

    Change to another website

--- WebGame01/public/js/t.introduce.js
+++
@@ -1,5 +0,0 @@
-const startBtn = document.getElementById('startBtn')
-
-startBtn.addEventListener("click", () => {
-    location.href = "./text.html"
-})
--- WebGame01/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/**/*np.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
